7aae43f3f7dda3e81c18879d490bebeaf0a5d147
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-17 04:32:11 +0200

    Created class Date and changed property 5 in the class Employee

--- WorkerLib/Class1.cs
+++ WorkerLib/Class1.cs
@@ -8,9 +8,15 @@ namespace WorkerLib
         public string FirstName { set; get; }
         public string Surname { set; get; }
         public string Place { set; get; }
-        public string Date { set; get; }
+        public Date Date { set; get; }
         public string Sex { set; get; }
         public string NumberOfPeopleInTheFamily { set; get; }
        
+    }
+    public class Date
+    {
+        public int Day { set; get; }
+        public int Mounth { set; get; }
+        public int Year { set; get; }
     }
 }
