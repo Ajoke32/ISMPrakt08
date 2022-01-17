4ab417d985ae5dc6c795b2afab76ce099f5970e3
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-17 04:28:29 +0200

    Created class Employee

---
+++ WorkerLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WorkerLib
+{
+    public class Employee
+    {
+        public string Name { set; get; }
+        public string FirstName { set; get; }
+        public string Surname { set; get; }
+        public string Place { set; get; }
+        public string Date { set; get; }
+        public string Sex { set; get; }
+        public string NumberOfPeopleInTheFamily { set; get; }
+       
+    }
+}
